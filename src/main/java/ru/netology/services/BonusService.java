ce1b982899907fe9f68a0b921a15296a36e39c0a
Stas Git <yolkhingp@gmail.com>
2024-12-28 15:06:16 +0300

    transferred resources

--- src/main/java/ru/netology/services/BonusService.java
+++ src/main/java/ru/netology/services/BonusService.java
@@ -5,7 +5,7 @@ public class BonusService {
     public int calculate(int income, int expenses) {
         int count = 0; // счётчик месяцев отдыха
         int money = 0; // количество денег на счету
-        int threshold = 20_000; //порог денег для отдыха
+        //int threshold = 20_000; //порог денег для отдыха
         for (int month = 0; month < 12; month++) {
             if (money >= expenses) { // можем ли отдыхать?
                 count++; // увеличиваем счётчик месяцев отдыха
